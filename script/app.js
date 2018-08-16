36f3c2ab8daad28676ba5958370fd884eb0d9990
Bhadraporn Indraraksha <cisdbhin@th.ibm.com>
2018-08-17 00:36:04 +0700

    try to add angular js

---
+++ script/app.js
@@ -0,0 +1,7 @@
+angular.module("app",[]).controller("mainCtrl",function(){
+    var ctrl = this; 
+    ctrl.name = "Fern"; 
+    ctrl.position = "Trying so hard to be a web developer."; 
+    ctrl.concern = "Constantly wonder if she is trying hard enough.";
+
+});
