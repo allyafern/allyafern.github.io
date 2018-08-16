7ce4a00af39f2c4a0bbe2c21430ef554ebd1a80a
Bhadraporn Indraraksha <cisdbhin@th.ibm.com>
2018-08-17 00:49:00 +0700

    try to change a message

--- script/app.js
+++ script/app.js
@@ -2,6 +2,6 @@ angular.module("app",[]).controller("mainCtrl",function(){
     var ctrl = this; 
     ctrl.name = "Fern"; 
     ctrl.position = "Trying so hard to be a web developer,"; 
-    ctrl.concern = "constantly wondering if I'm trying hard enough.";
+    ctrl.concern = "constantly wondering if I'm trying hard enough. ";
 
 });
